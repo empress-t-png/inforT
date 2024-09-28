a5a4981c3b1cac1874f66b35589c48b3603b46f5
empressT <tosin.e@northeastern.edu>
2024-09-27 21:42:15 -0400

    first commit

---
+++ TopicCLecture/src/topicclecture/Graduate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package topicclecture;
+
+/**
+ *
+ * @author Conestoga
+ */
+public class Graduate extends Student{
+    
+    String sid = "123";
+
+    
+}
